278cb8f992b8869960fb4eca36780793b7749228
yphill <phelipequeiroz2004@gmail.com>
2022-12-01 20:22:55 -0300

    modelo

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.etecia.listafilmes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +20,56 @@ public class MainActivity extends AppCompatActivity {
 
         listaFilmes = findViewById(R.id.idlistaFilmes);
 
+        MyAdapter adapter  = new MyAdapter();
+
         //preparar para instanciar o adaptador
         listaFilmes.setAdapter(Adaptador);
 
     }
+    //criando uma inner class MyAdapter
+
+    public class MyAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
